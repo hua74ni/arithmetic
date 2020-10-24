112005880e366e123c74c5b5bcc8d4296368ae09
huangdh <591327356@qq.com>
2020-10-24 11:36:27 +0800

    add 92. ReverseBetween algorithm problems and resolve on leetcode website

--- src/main/java/com/arithmetic/leetcode/ReverseBetween.java
+++ src/main/java/com/arithmetic/leetcode/ReverseBetween.java
@@ -20,10 +20,53 @@ public class ReverseBetween {
     }
 
     public ListNode reverseBetween(ListNode head, int m, int n) {
+        if (m == n) return head;
 
+        ListNode res = new ListNode(-1);
+        ListNode cur = head;
+        ListNode pre = null;
+        int index = 1;
+        while (head != null) {
+            if (index == m) {
+                if (index != 1) {
+                    res.next = head;
+                }
+                break;
+            }
+            pre = cur;
+            cur = cur.next;
+            index++;
+        }
 
+        ListNode preTail = pre;
+        ListNode reverseTail = cur;
+        ListNode reverseHead = null;
 
-        return head;
+        pre = cur;
+        cur = cur.next;
+        index++;
+        ListNode next = null;
+        while (cur != null) {
+            next = cur.next;
+            cur.next = pre;
+            if (index == n) {
+                reverseHead = cur;
+                break;
+            }
+            pre = cur;
+            cur = next;
+            index++;
+        }
+
+        reverseTail.next = next;
+        if (preTail != null) {
+            preTail.next = reverseHead;
+        }
+
+        if (m == 1) {
+            res.next = reverseHead;
+        }
+        return res.next;
     }
 
     public static void main(String[] args) {
@@ -39,7 +82,12 @@ public class ReverseBetween {
         head3.next = head4;
         head4.next = head5;
 
-        System.out.println(new ReverseBetween().reverseBetween(head1, 2, 4));
+        ListNode res = new ReverseBetween().reverseBetween(head1, 1, 2);
+
+        while (res != null) {
+            System.out.print(res.val + " -> ");
+            res = res.next;
+        }
     }
 
 }
